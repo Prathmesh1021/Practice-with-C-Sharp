1757ad143fbc36a5e5b7c03db9fa306f3f1a4984
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-14 14:47:33 +0530

    User Define Eception

--- csharpp/Exception handling negative number exceptipn Example5.cs
+++ csharpp/Exception handling negative number exceptipn Example5.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace csharpp
 {
-    internal class Exception_handling_negative_number_exceptipn_Example5:Exception
+    internal class Exception_handling_negative_number_exceptipn_Example5 : Exception
     {
+
+        private string _message="Enter only positive Number";
+
+        public string Message
+        {
+            get { return _message; }
+        }   
     }
 }
