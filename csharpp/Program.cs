9a952f19a53ecf7d7cbaa3143af520a5e8e463d8
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-14 18:34:03 +0530

    all object file added only for understanding

--- csharpp/Program.cs
+++ csharpp/Program.cs
@@ -6,7 +6,56 @@ namespace csharpp
     internal class Program
     {
         static void Main()
+
+                
         {
+            /// File Handling Example6  // create, write, append, read file using short method and try catch    
+            {
+                SHORT_METHOD_FileHadlingCWARD_Using_Exception_try_catch_Example6 obj12 = new SHORT_METHOD_FileHadlingCWARD_Using_Exception_try_catch_Example6();
+                obj12.createfile();
+                obj12.addContentOnFile();
+                obj12.appendContentOnFile();
+                obj12.readFileOperation();
+            }
+
+
+
+            /// File Handling Example4  // create, write, append, read file using try catch 
+            {
+                FileHadlingCWARD_Using_Exception_try_catch_Example4 obj14 = new FileHadlingCWARD_Using_Exception_try_catch_Example4();
+                //obj.readFileOperation();    
+
+                obj14.createfile();
+                obj14.addContentOnFile();
+                obj14.appendContentOnFile();
+                obj14.readFileOperation();
+
+            }
+
+
+
+            /// File Handling Example1  // create and write on file 
+            {
+                FileHandlingExample1 obj = new FileHandlingExample1();
+                obj.createfile();
+            }
+
+            /// File Handling Example2  // create, write and append on file
+            {
+                FileHandlingExample2_Append obj13 = new FileHandlingExample2_Append();
+                obj13.createfile();
+                 obj13.addContentOnFile();
+                 obj13.appendContentOnFile();
+            }
+
+            /// File Handling Example3  // read and delete file   
+            {
+                FileHandlingExample3_Read obj1 = new FileHandlingExample3_Read();
+
+                obj1.readFileOperation();
+                obj1.deleteFileOperation();
+            }
+
 
             ///6.2 Arithmatic operations using class and object Add,Sub,Mul,Div 
             static void Main()
@@ -43,35 +92,35 @@ namespace csharpp
                 Datatype D = new Datatype();
                 D.PDatatype();
                 D.DDatatype();
-            }  
+            }
 
             { ///29.2 Employee class using class and object Example 
                 EmployeeForClass obj1 = new EmployeeForClass();
-                obj1.Accept(1, "John", 50000);  
-                obj1.Display(); 
+                obj1.Accept(1, "John", 50000);
+                obj1.Display();
 
             }
             ///31.2 Member function Parameter 1 No parameter no return type Example 
             {
                 MemberFunction_Parameter1_no_parameter_no_return_type obj3 = new MemberFunction_Parameter1_no_parameter_no_return_type();
-                obj3.squre();   
+                obj3.squre();
             }
             ///33.2 Member function Parameter 4 Parameter with return Type Example  
             {
                 MemberFunction_Parameter_4_Parameter_with_return_Type obj4 = new MemberFunction_Parameter_4_Parameter_with_return_Type();
                 int d = obj4.squre(32);
-                Console.WriteLine(d);    
+                Console.WriteLine(d);
             }
             { ///34.2 Constructor Example1  Default Constructor 
                 ConstructorExample1 obj5 = new ConstructorExample1(12);
                 { }
             }
             { ///35.2 Constructor Example2  Parameterized Constructor 
-                ConstructorExample2 obj6 = new ConstructorExample2(4,5);
+                ConstructorExample2 obj6 = new ConstructorExample2(4, 5);
                 { }
             }
             { ///36.2 Constructor Example3  Copy Constructor 
-                ConstructorExample3 obj7 = new ConstructorExample3(12,17);
+                ConstructorExample3 obj7 = new ConstructorExample3(12, 17);
                 ConstructorExample3 obj8 = new ConstructorExample3(obj7);
                 { }
             }
@@ -96,12 +145,12 @@ namespace csharpp
 
             }
             ///41.3 inheritance object reference Example2  Multilevel Inheritance  
-           
-                { 
-            AbstractClassExample1 obj7 = new AbstractCHILDClass2();  
+
+            {
+                AbstractClassExample1 obj7 = new AbstractCHILDClass2();
                 obj7.Accept(12, 32);
                 obj7.rectangle();
-                obj7.Display(); 
+                obj7.Display();
 
                 AbstractClassExample1 obj8 = new AbstractCHILDClass2();
                 obj8.Accept(12, 3);
@@ -119,10 +168,10 @@ namespace csharpp
             {
                 ///43.3 interface object reference Example3  Interface   
                /* FIRST obj10 = new INTERFACE_implement_Example3();
-                obj10.fun1();   
-                obj10.fun2();   
+                obj10.fun1();
+                obj10.fun2();
 
-                SECOND obj11= new INTERFACE_implement_Example3();
+                SECOND obj11 = new INTERFACE_implement_Example3();
                 obj11.fun1();
                 obj11.fun2();
                 obj11.fun3();*/
@@ -141,7 +190,7 @@ namespace csharpp
                     num = Convert.ToInt32(Console.ReadLine());
 
                     if (num < 0)
-                    {
+                    { 
 
                         throw new Exception_handling_negative_number_exceptipn_Example5();
 
