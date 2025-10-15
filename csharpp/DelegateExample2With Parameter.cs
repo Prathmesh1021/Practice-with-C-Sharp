7b5e81ac4686cba4756b2d887470acb87197bb92
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-15 22:26:30 +0530

    Delegate With Parameter

---
+++ csharpp/DelegateExample2With Parameter.cs
@@ -0,0 +1,32 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class DelegateExample2With_Parameter
+    {
+
+        void Welcome1(String msg)//with parameter    
+        {
+
+            Console.WriteLine(msg);
+        }
+        public delegate void WelcomeHandler(String msg);//with parameter    
+
+        static void Main()
+        {
+            DelegateExample2With_Parameter obj = new DelegateExample2With_Parameter();
+            WelcomeHandler handler = new WelcomeHandler(obj.Welcome1);
+            handler("Welcome In C#");
+            handler("Welcome In .NET");
+            handler("Welcome in Java");
+            handler("Welcome In Paythan");
+            handler("Welcome in C");
+            handler("Welcome in C++");
+
+        }
+    }
+}
