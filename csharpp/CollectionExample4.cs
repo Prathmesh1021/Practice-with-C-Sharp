7ea41eb56644a398bf567dc159747cd1d297826e
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-15 13:59:46 +0530

    Collection Stack Queue

---
+++ csharpp/CollectionExample4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;   
+
+namespace csharpp
+{
+    internal class CollectionExample4
+    {
+        internal void StackFun()
+        {
+
+            Stack Sk = new Stack();
+            Sk.Push("playear 1001");
+            Sk.Push("playear 1004");
+            Sk.Push("playear 1005");
+            Sk.Push("playear 1009");
+            Sk.Push("playear 1010");
+
+             Console.WriteLine(Sk.Pop());//1010  
+             Console.WriteLine(Sk.Peek());//1009   
+            while (Sk.Count != 0)
+            {
+
+                Console.WriteLine(Sk.Pop());//
+
+            }
+
+        }
+       internal void QueueFub() {
+            Queue Qu = new Queue();
+            Qu.Enqueue("playear 1001");
+            Qu.Enqueue("playear 1004");
+            Qu.Enqueue("playear 1005");
+            Qu.Enqueue("playear 1009");
+            Qu.Enqueue("playear 1010");
+
+             Console.WriteLine(Qu.Dequeue());//1010  
+            Console.WriteLine(Qu.Peek());//1009   
+           while (Qu.Count != 0)
+            {
+
+                Console.WriteLine(Qu.Dequeue());//
+         }
+            
+
+
+
+
+        }
+    }
+    
+}
+
