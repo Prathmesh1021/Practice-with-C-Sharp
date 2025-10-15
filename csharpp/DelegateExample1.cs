461385158d28bd1ba4acd8f7eadc2c015781fb35
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-15 22:25:54 +0530

    Delegate Without Parameter

---
+++ csharpp/DelegateExample1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class DelegateExample1
+    {
+
+        void Welcome()//without parameter
+        { 
+        
+        Console.WriteLine("Welcome to C# Programming");
+        }
+        public delegate void WelcomeHandler();//without parameter   
+
+        static void Main()
+        {
+            DelegateExample1 obj = new DelegateExample1();
+            WelcomeHandler handler = new WelcomeHandler(obj.Welcome);
+            handler();
+            handler();
+            handler();
+            handler();
+            handler();
+            handler();
+
+        }
+    }
+}
