1662b662f8db0694686037eb6c267288341d8dc7
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-15 12:24:42 +0530

    CollectionHashTable

---
+++ csharpp/CollectionExample2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class CollectionExample2
+    {
+        internal void HasdhtableFun() {
+        
+        Hashtable Ht = new Hashtable();
+            Ht.Add("Roll No.",1001);
+            Ht.Add("Name","John the Don");
+            Ht.Add("Branch", "ECS");
+            Ht.Add("Fees",120000);
+
+            foreach (DictionaryEntry Obj in Ht)
+            {
+                Console.WriteLine("Key is {0} and value is {1}",Obj.Key,Obj.Value);
+            
+            }
+
+        }
+    }
+}
