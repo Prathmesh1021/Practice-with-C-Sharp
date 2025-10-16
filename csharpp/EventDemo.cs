48dd73a9cd69b5c22347eb5c6f4c58c35d300c27
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-16 13:00:31 +0530

    EventExample

---
+++ csharpp/EventDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class EventDemo
+    {
+        public delegate void EventHandler(); //create delegate
+        public event EventHandler MyEvent // create event with add and remove event
+        {
+            add
+            {
+                Console.WriteLine("add operation");
+            }
+            remove
+            {
+                Console.WriteLine("remove operation");
+            }
+        }
+    }
+}
