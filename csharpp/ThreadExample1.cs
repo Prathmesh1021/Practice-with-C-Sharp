e9ca1556a7bb4e89e4bdd16210cb095a8e31661c
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-16 13:59:25 +0530

    Single Thread Example

---
+++ csharpp/ThreadExample1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ThreadExample1
+    {
+        void Display() {
+            for (int i = 1; i <= 10; i++) {
+
+                Console.WriteLine("process " + i);
+                Thread.Sleep(10000);
+
+            }
+        }
+
+        public static void Main()
+        {
+            ThreadExample1 obj = new ThreadExample1();
+            ThreadStart th=new ThreadStart(obj.Display);   
+            Thread t = new Thread(th);  
+            t.Start();  
+
+        }   
+
+    }
+}
