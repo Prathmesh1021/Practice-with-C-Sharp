332dde18a1cf4b94cecb9f5c7e1b2fbdc6e8af92
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-17 13:42:25 +0530

    Add program to demonstrate object array storing multiple data types

---
+++ csharpp/ArrayExample11 ObjectA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ArrayExample11_ObjectA
+    {
+        static void Main()
+        {
+            object[] arr = { "C", "CPP", 1200, 12.34F, true, 'a' };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
