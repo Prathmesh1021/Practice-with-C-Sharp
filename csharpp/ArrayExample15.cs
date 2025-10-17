ca280ef7d6c33bff831f10bd5fc06147939b4f31
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-17 14:06:43 +0530

    Merge two arrays into one array

---
+++ csharpp/ArrayExample15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ArrayExample15
+    {//Q)  WAP to merge two arrays in one array?
+        static void Main()
+        {
+            int[] a = { 1, 2 };
+            int[] b = { 3, 4 };
+            int[] c = new int[a.Length + b.Length];
+            int index = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                c[index] = a[i];
+                index++;
+            }
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                c[index] = b[i];
+                index++;
+            }
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.WriteLine(c[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
