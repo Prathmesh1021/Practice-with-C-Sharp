cd715c435b35e51d42119ee589e9d5218279e629
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-17 14:05:12 +0530

    Sort array in ascending order

---
+++ csharpp/ArrayExample14.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace csharpp
+{
+    internal class ArrayExample14
+    {
+        // Q)  WAP to sort the elements of the array?
+        static void Main()
+        {
+            int[] arr = { 10, 11, 23, 34, 67, 89, 2, 67 };
+            int c;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        c = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = c;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted array is ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
