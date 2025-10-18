0a2768aa54531975f3ee8f0cc4ba1abb2910af59
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-18 23:43:21 +0530

    Add char array reverse printing

---
+++ csharpp/ArrayExample22.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ArrayExample22
+    {
+        static void Main(string[] args)
+        {
+            char[] ar = { 'A', 'B', 'C', 'D', 'E', 'X', 'Y', 'Z' };
+
+            Console.WriteLine("Reverse of character array:");
+            for (int i = ar.Length - 1; i >= 0; i--)
+            {
+                Console.Write(ar[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
