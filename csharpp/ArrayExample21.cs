0a53d450ee0b25b0490a8d01dbd11ec47150056b
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-18 23:40:34 +0530

    descending order sort of int array

---
+++ csharpp/ArrayExample21.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ArrayExample21
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            int i, j, temp;
+            for (i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine("enter element of array" + i + "Index");
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < Arr.Length; i++)
+            {
+                for (j = i + 1; j < Arr.Length; j++)
+                {
+                    if (Arr[i] < Arr[j])
+                    {
+                        temp = Arr[i];
+                        Arr[i] = Arr[j];
+                        Arr[j] = temp;
+                    }
+                }
+            }
+            for (i = 0; i <= Arr.Length; i++)
+            {
+                Console.WriteLine("sort of array is " + Arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
