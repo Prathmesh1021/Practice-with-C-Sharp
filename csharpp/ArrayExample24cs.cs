7c7dc83e0b985f246c56c394ba763185a9f89a14
Prathmesh Patil <ppatil1021@gmail.com>
2025-10-18 23:53:22 +0530

    Add array even-odd sum example

---
+++ csharpp/ArrayExample24cs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpp
+{
+    internal class ArrayExample24cs
+    {
+        static void Main() {
+
+            int[] A = { 1, 2, 3, 4, 5, 6, 7 };
+            int SE = 0, SO = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    SE = SE + A[i];
+                }
+                else
+                {
+                    SO = SO + A[i];
+                }
+            }
+            Console.WriteLine("Sum of Odd : {0} \nSum of Even {1}", SO, SE);
+        }
+    }
+    }
+
